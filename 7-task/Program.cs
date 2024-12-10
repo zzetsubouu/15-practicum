d7c38fabb4cd6ce5d2112784e50b018e2ef0d946
zzetsubouu <154281553+zzetsubouu@users.noreply.github.com>
2024-12-10 11:15:56 +0200

    Add files via upload

---
+++ 7-task/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = { "Tom", "Sam", "Bob", "Kate", "Tom", "Alice"};
+
+            Array.Sort(people, 1, 3);
+            foreach (var item in people)
+            {
+                Console.WriteLine(item);
+            }
+
+            //string first = Array.Find(people, person => person.Length > 3);
+            //Console.WriteLine(first);
+            //string last = Array.FindLast(people, person => person.Length > 3);
+            //Console.WriteLine(last);
+            //string[] all = Array.FindAll(people, person => person.Length <= 3);
+            //foreach (var item in all)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ 8-task/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_task
+{
+    /// <summary>
+    /// Задача 10. Удаление всех четных элементов из массива, и в новом массиве сложить элементы.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = { 3, 2, 7, 5, 4, 8, 1, 9, 5 };
+            omas = omas.Where(x => x % 2 != 0).ToArray();
+            foreach (var item in omas)
+            {
+                Console.WriteLine(item);
+            }
+            int sum = omas.Sum();
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
